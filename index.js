965a55d9c0e33bcc4fb500e9628b30f0e380c09d
Ryan Street <ryanstreet122@gmail.com>
2023-02-02 14:42:37 -0600

    imports into api index

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose')
+
+
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const cwd = process.cwd();
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.unsubscribe(routes);
+app.use(routes);
 
 db.once('open', () => {
     app.listen(PORT, ()=> {
